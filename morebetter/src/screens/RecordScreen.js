c4f93a231a7e80300bdccdd3f0268a90e8eb73bc
remagart <jfmamjjasond418@gmail.com>
2020-07-13 01:04:17 +0800

    [Add] 新增樣式
    20200712

--- morebetter/src/screens/RecordScreen.js
+++ morebetter/src/screens/RecordScreen.js
@@ -2,8 +2,10 @@ import React, { useEffect,useState } from 'react'
 import { Text, View,StyleSheet,FlatList } from 'react-native';
 import {Fib} from "../utils/forgetting_curve/Fibonacci";
 import DateHelper from '../utils/common/DateHelper';
+import CommonStyle from "../style/CommonStyle";
 
 const DAY11 = "2020-07-06";
+const InitDay = 11;
 const NOW = DateHelper.getOptionalTimeFormatString(DateHelper.currentTimeMillis());
 const DUR = DateHelper.calculateDurationMin(DAY11,NOW,"days");
 
@@ -14,13 +16,21 @@ const RecordScreen = (props) => {
         let day = Fib(DUR);
         setdayData(day);
     },[]);
+
+    const renderTitle = () => {
+        return (
+            <View style={styles.titleView}>
+                <Text style={styles.titleTxt}>已經開始第 {DUR} 天了</Text>
+            </View>
+        );
+    }
     
     const renderBlock = (item) => {
         let target = DateHelper.addDayFormatString(NOW,(0-item),"YYYY-MM-DD");
         
         return (
-            <View style={{backgroundColor: "#DDFFAA"}}>
-                <Text>{target}: DAY{DUR-item}</Text>
+            <View style={styles.blockView}>
+                <Text>{target}: DAY{DUR-item+InitDay}: </Text>
             </View>
         );
     }
@@ -28,10 +38,18 @@ const RecordScreen = (props) => {
     const renderList = () => {
         return(
             <FlatList 
+                ListHeaderComponent={renderTitle()}
                 keyExtractor = {(item,index)=> "RecordScreen"+String(item)}
                 data = {dayData}
                 extraData = {dayData}
-                renderItem = {({item})=>renderBlock(item)}
+                renderItem = {({item})=>{
+                    return(
+                        <View>
+                            {renderBlock(item)}
+                            <View style={styles.line}/>
+                        </View>
+                    )
+                }}
             />
         );
     }
@@ -46,6 +64,27 @@ const RecordScreen = (props) => {
 const styles = StyleSheet.create({
     container:{
         flex:1,
+        backgroundColor: CommonStyle.backgroungColor.color,
+    },
+    titleTxt:{
+        ...CommonStyle.textBody2,
+        fontWeight: "bold",
+    },
+    titleView: {
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#e7acbc"
+    },
+    blockView: {
+        height: 40,
+        justifyContent: "center",
+        backgroundColor: "#FFF",
+        paddingHorizontal: 16
+    },
+    line: {
+        backgroundColor: CommonStyle.separatorColor.color,
+        height: 1,
     }
 })
 
---
+++ morebetter/src/style/CommonStyle.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+
+export default CommonStyle = StyleSheet.create({
+    headerTitleStyle : {
+        color: '#000000CC',
+        fontSize: 19,
+        textAlign: 'center',
+        alignSelf: 'center',
+        marginHorizontal: 0,
+        width: '100%',
+    },
+    mainColor:{color: "#14bdcc"},
+    secondColor: {},
+    thirdColor: {},
+    backgroungColor:{color: "rgb(242,242,242)"},
+    text_weight: {color: "rgba(0,0,0,0.8)"},
+    text_normal: {color: "rgba(0,0,0,0.6)"},
+    text_light: {color: "rgba(0,0,0,0.45)"},
+    separatorColor: {color: "#E6E6E6"},   // rgb(230,230,230)
+    errMsgColor: {color: "#f24030"},    // rgb(rgb(242,64,48))
+    textH1:{
+        fontSize: 28,
+        lineHeight: 37,
+    },
+    textH2:{
+        fontSize: 25,
+        lineHeight: 33,
+    },
+    textH3:{
+        fontSize: 22,
+        lineHeight: 29,
+    },
+    textH4:{
+        fontSize: 19,
+        lineHeight: 25,
+    },
+    textBody1:{
+        fontSize: 17,
+        lineHeight: 23,
+    },
+    textBody2:{
+        fontSize: 15,
+        lineHeight: 20,
+    },
+    textCaption:{
+        fontSize: 13,
+        lineHeight: 18,
+    },
+    textSmallest:{
+        fontSize: 11,
+        lineHeight: 15,
+    },
+    
+})
